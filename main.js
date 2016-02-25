08b20bb0ab34176510bed7d3041dfce69f584fa3
kb <kiel.j.barry@gmail.com>
2016-02-25 10:06:11 -0800

    plays tic tac toe

--- main.js
+++ main.js
@@ -1,82 +1,110 @@
 
 
-document.addEventListener('DOMContentLoaded',function(){
 
-	var inputName = [];
-	var ul = document.getElementById('addToMe');
-	var newContent =[];
-	var newLi = [];
-	var fullArray = [];
-	var luckyNumber = 0;
-	var luckyStudent = [];
-	var p = document.getElementById('addRand');
-	console.log(p);
-
-	// console.log(ul);
-
-	document.addEventListener('click', function(event){
-	//this does primary button/list
-		if(event.target.classList.contains('btn-primary')){
-			//this logs the input value when button is clicked;
-			console.log("button clicked");
-			inputName = document.getElementsByTagName('input')[0].value.split(", ");
-			console.log(inputName);
-
-			//this will create new LI in UL;
-			for (var i = 0; i<inputName.length; i++){
-				console.log('second click function!');
-				newLi = document.createElement('li');
-				newContent = document.createTextNode(inputName[i]);
+//need to review the checkWinner function and see wether its placement without the addEventListener function
+// is it being called too often?
 
 
-				newLi.appendChild(newContent);
-				console.log("newContent",newContent);
-				console.log("newLi:",newLi);
-				ul.appendChild(newLi);
-
-				fullArray.push(inputName[i]);
-				console.log("namePushed",inputName[i]);
+document.addEventListener('DOMContentLoaded', function(){
+	var totalCount = -1;
+	var xArray = [];
+	var oArray = [];
+	var winningArrays = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[3,5,7],[1,5,9]];
+	var xArrayVsWinArr = [];
+	var xCount = 0;
+	var oArrayVsWinArr = [];
+	var oCount = 0;
+	var winner = "";
+	//assigns winner to variable winner
+	var checkWinner = function(){
+		//creates array of x counts against winning combos
+		winningArrays.map(function(x){
+			x.map(function(y){
+				for (var i =0; i<xArray.length; i++){
+					if (xArray[i] == y){
+						xCount++;
+					}
+				}
+				xArrayVsWinArr.push(xCount);
+			});
+		});
+		//creates array of o counts against winning combos
+		winningArrays.map(function(x){
+			x.map(function(y){
+				for (var i =0; i<xArray.length; i++){
+					if (oArray[i] == y){
+						oCount++;
+					}
+				}
+				oArrayVsWinArr.push(oCount);
+			});
+		});
+		//checks and assigns winner if x array has winners
+		xArrayVsWinArr.map(function(x){
+			if (x > 2){
+				winner = "player1";
 			}
-		document.getElementById('eraseMe').value="";
-		console.log("fullArray:",fullArray);
+		});
+		//checks and assigns winner if o array has winners
+		oArrayVsWinArr.map(function(x){
+			if (x>2){
+				winner = "player2";
+			}
+		});
+	};
+	document.addEventListener('click',function(event){
+// checks if box clicked
+		if (event.target.classList.contains('box')){
+			// 'togglers' the player
+			if (totalCount % 2 !== 0){
+				///list player whose turn it is
+				var playerListingSpot = document.getElementById("playerUp");
+				playerListingSpot.innerHTML = 'player1'
+			}
+			else {
+				var playerListingSpot = document.getElementById("playerUp");
+				playerListingSpot.innerHTML = 'player2'
+			}
+			//changes HTML content of clicked divs to X or O;
+			totalCount++;
+			if (totalCount % 2 === 0) {
+					var id = event.target.id;
+					console.log(id);
+					var elOfId = document.getElementById(id);
+					var placeToChange = elOfId.innerHTML = 'X';
+					xArray.push(id)
+					xArray.sort();
+					console.log(xArray);
+			}
+			else {
+					var id = event.target.id;
+					console.log(id);
+					var elOfId = document.getElementById(id);
+					var placeToChange = elOfId.innerHTML = 'O';
+					oArray.push(id)
+					oArray.sort();
+					console.log(oArray);
+			}
+			checkWinner();
+			console.log("xArrayWinArr",xArrayVsWinArr);
+			console.log("xCount",xCount);
+			console.log("oArrayVSWinArr",oArrayVsWinArr);
+			console.log("oCount",oCount);
+			console.log("winner",winner);
+			var winnersDiv = document.getElementById('theWinner');
+			winnersDiv.innerHTML = "The winner is" + winner;
 		}
-
-// moved this section up
-		if (event.target.id=='randomizer'){
-			console.log("crazy random picked!");
-			luckyNumber = Math.floor((Math.random()*fullArray.length));
-			console.log(luckyNumber);
-
-			luckyStudent = document.createTextNode(fullArray[luckyNumber]);
-			console.log(luckyStudent);
-			p.appendChild(luckyStudent);
+	//if reset button clicked, set innerHTML to ID;
+	//reset button also needs to reset the values for the variables
+	if (event.target.classList.contains('resetter')){
+		var resetterArray = document.getElementsByClassName('box');
+		for (var i = 0; i < resetterArray.length; i++){
+			resetterArray[i].innerHTML = i;
+			console.log("is this id",i);
 		}
-
+	}
 	});
-	console.log("ul",ul);
-
-	// picks a random number between 0 and amount of students
-	// document.querySelector('#randomizer').onclick = function(){
-	// 	console.log("crazy random picked!");
-	// 	luckyNumber = Math.floor((Math.random()*fullArray.length)+1);
-	// 	console.log(luckyNumber);
-
-	// 	luckyStudent = document.createTextNode(fullArray[luckyNumber]);
-	// 	console.log(luckyStudent);
-	// 	p.appendChild(luckyStudent);
 });
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
